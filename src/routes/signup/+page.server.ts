cef47d0b8e71f08b465247c01806588fc01efc15
Ross Keenan <rossk29@gmail.com>
2023-02-12 12:18:02 +0200

    Create the Email Verification OTP

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,3 +1,4 @@
+import { OTPs } from '$lib/models/OTPs';
 import { auth } from '$lib/server/lucia';
 import { error, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
@@ -10,7 +11,7 @@ export const load: PageServerLoad = async ({ locals }) => {
 };
 
 export const actions: Actions = {
-    default: async ({ request, locals }) => {
+    default: async ({ request, locals, url }) => {
         const form = await request.formData();
         const email = form.get('email');
         const password = form.get('password');
@@ -21,7 +22,7 @@ export const actions: Actions = {
         }
 
         try {
-            const user = await auth.createUser({
+            const { userId } = await auth.createUser({
                 key: {
                     // The authentication provider we're using
                     providerId: 'email',
@@ -30,12 +31,27 @@ export const actions: Actions = {
                     password
                 },
                 attributes: {
-                    email
+                    email,
+                    emailVerified: false
                 }
             });
 
+
+            // If successful, we know there no existing email-verification OTPs,
+            //   since we just created the user.
+            //   So we can create a new one without checking for existing ones.
+            const otp = await OTPs.create({
+                userId,
+                kind: 'email-verification',
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24)
+            });
+            const href = `${url.origin}/api/verify-email?token=${otp.token}`;
+            console.log(href);
+            console.log('TODO: sendEmail');
+
+
             // Create and set the session cookies
-            const session = await auth.createSession(user.userId);
+            const session = await auth.createSession(userId);
             locals.setSession(session);
         } catch {
             // Email already taken
